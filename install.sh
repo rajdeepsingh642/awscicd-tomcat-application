cd90898cf2af13e7942487bc8ece6503e142590b
rajdeepsingh642 <rajdeepsingh642@gmail.com>
2024-08-17 17:02:55 +0530

    maven233

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/my-webapp.war/sample-java-projects/my-webapp.war /tmp
+aws s3 cp s3://java-artifacts-devops4solutions/tomcat/target/my-webapp.war/ /tmp
 sudo mv /tmp/my-webapp.war /usr/share/tomcat/webapps/my-webapp.war
 sudo service tomcat restart
