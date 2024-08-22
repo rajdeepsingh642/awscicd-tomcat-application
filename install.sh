36519d5db54ca58d69d33a38f3b86b9bfea67de7
rajdeepsingh642 <rajdeepsingh642@gmail.com>
2024-08-22 11:12:24 +0530

    all

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-aws s3 cp s3://java-artifacts-devops4solutions/tomcat/target/my-webapp.war/ /tmp
-sudo mv /tmp/my-webapp.war /opt/tomcat/webapps/my-webapp.war
-sudo service tomcat restart
+#!/bin/bash
+systemctl stop tomcat
+rm -rf /opt/tomcat/webapps/my-webapp
+systemctl start tomcat
